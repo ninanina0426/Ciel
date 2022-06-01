2114def07531f06d4e059996b65622774ca1b586
chii0526 <2016005@s.asojuku.ac.jp>
2022-06-01 09:57:20 +0900

    aitemu

--- ciel/scene/Obj/Aitem.cpp
+++ ciel/scene/Obj/Aitem.cpp
@@ -1,6 +1,6 @@
-#include "Aitem.h"
 #include <DxLib.h>
-
+#include"../../stage/StageMng.h"
+#include "Aitem.h"
 
 
 Aitem::Aitem()
@@ -18,8 +18,8 @@ bool Aitem::init()
 
 	ItemAnimcount = 0;
 
-	mPos.x_ = 300;
-	mPos.y_ = 200;
+	mPos.x_ = 0;
+	mPos.y_ = 0;
 	mSize.x_ = 32;
 	mSize.y_ = 32;
 
@@ -53,103 +53,121 @@ Vector2 Aitem::Update(void)
 
 void Aitem::Draw(Vector2 mMapoffset)
 {
+	MAP_ID mapID = lpMapMng.GetMapId();
+
+	if (mapID == MAP_ID::SWEETS)
+	{
+		if (mKami1 == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mKami2 == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 50, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+	}
+	if (mapID == MAP_ID::SWEETSOUT)
+	{
+		if (mKami3 == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 100, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mKami4 == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 150, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+	}
+	if (mapID == MAP_ID::FOREST)
+	{
+		if (mKami5 == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 200, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mTma == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 250, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+	}
+	if (mapID == MAP_ID::CAVE)
+	{
+		if (mMasinngan == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 300, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mHoutai == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_, mPos.y_ - 10 - mMapoffset.y_ + 50, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mHeal == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 50, mPos.y_ - 10 - mMapoffset.y_ + 50, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mKey == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 100, mPos.y_ - 10 - mMapoffset.y_ + 50, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mbook == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 150, mPos.y_ - 10 - mMapoffset.y_ + 50, mImage[0][mAnimCnt], true);
+			}
+		}
+		if (mMagazinn == true)
+		{
+			int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
+			if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
+			{
+				DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 200, mPos.y_ - 10 - mMapoffset.y_ + 50, mImage[0][mAnimCnt], true);
+			}
+		}
+	}
 	
-	if (mKami1==true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mKami2 == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_+50, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mKami3 == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_+100, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mKami4 == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 150, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mKami5 == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_+200, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mTma == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 250, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mMasinngan == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_+300, mPos.y_ - 10 - mMapoffset.y_, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mHoutai == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_ , mPos.y_ - 10 - mMapoffset.y_+50, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mHeal == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_+50, mPos.y_ - 10 - mMapoffset.y_+50, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mKey == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 100, mPos.y_ - 10 - mMapoffset.y_+50, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mbook == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_+150, mPos.y_ - 10 - mMapoffset.y_+50, mImage[0][mAnimCnt], true);
-		}
-	}
-	if (mMagazinn == true)
-	{
-		int mAnimCnt = abs((ItemAnimcount / 7 % 4) - 2);
-		if ((mAnimCnt >= 0) && (mAnimCnt < ITEM_ANIM_MAX))
-		{
-			DrawGraph(mPos.x_ - 10 - mMapoffset.x_ + 200, mPos.y_ - 10 - mMapoffset.y_+50, mImage[0][mAnimCnt], true);
-		}
-	}
+	
+	
+	
+	
 }
 
 bool Aitem::Release(void)
